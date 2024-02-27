086113b506c1a6bcec40b2b5647ca4c37c17b368
talhatabrez <talhatabrezm@gmail.com>
2024-02-27 18:03:56 +0530

    Commit after writing .html and .js files seperately

---
+++ script.js
@@ -0,0 +1,21 @@
+var myAge = 21;
+
+var earlyYears = 2;
+earlyYears  *= 10.5;
+
+var laterYears = myAge - 2;
+laterYears *= 4;
+
+var myAgeInDogYears = earlyYears + laterYears;
+
+var myName = "Sayayy";
+myName = myName.toLowerCase(); 
+
+console.log("Early Years: " + earlyYears);
+console.log("Later Years: " + laterYears);
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
+
+
+document.write("<p>Early Years: " + earlyYears + "</p>");
+document.write("<p>Later Years: " + laterYears + "</p>");
+document.write("<p>My name is " + myName + ". I am " + myAgeInDogYears + " years old in dog years.</p>");
